316230f9e04bf54254b3b2ad99325aff1c03f953
Shriramana Sharma <samjnaa@gmail.com>
2018-08-01 20:01:07 +0530

    expose hue_steps, saturation and other cleanups
    hue_steps control useful mainly to change direction of rainbow
    saturation control useful to get more pastel vs vibrant colours
    no point in trivial setters/getters [this slightly breaks API :-(]
    simplify variable names
    remove misleading comments re hue range
    reduce hue variable type to 8-bits so we don't need to enforce 255 max value
    make hue_steps variable type signed to enable changing direction of rainbow

--- src/Kaleidoscope-LEDEffect-Rainbow.cpp
+++ src/Kaleidoscope-LEDEffect-Rainbow.cpp
@@ -4,60 +4,36 @@ namespace kaleidoscope {
 
 void LEDRainbowEffect::update(void) {
   uint16_t now = millis();
-  if ((now - rainbow_last_update) < rainbow_update_delay) {
+  if ((now - last_update) < update_delay) {
     return;
   } else {
-    rainbow_last_update = now;
+    hue += hue_steps;
+    last_update = now;
   }
 
-  cRGB rainbow = hsvToRgb(rainbow_hue, rainbow_saturation, rainbow_value);
-
-  rainbow_hue += rainbow_steps;
-  if (rainbow_hue >= 255) {
-    rainbow_hue -= 255;
-  }
+  cRGB rainbow = hsvToRgb(hue, saturation, brightness);
   ::LEDControl.set_all_leds_to(rainbow);
 }
-
-void LEDRainbowEffect::brightness(byte brightness) {
-  rainbow_value = brightness;
-}
-
-void LEDRainbowEffect::update_delay(byte delay) {
-  rainbow_update_delay = delay;
-}
-
 
 // ---------
 
 void LEDRainbowWaveEffect::update(void) {
   uint16_t now = millis();
-  if ((now - rainbow_last_update) < rainbow_update_delay) {
+  if ((now - last_update) < update_delay) {
     return;
   } else {
-    rainbow_last_update = now;
+    hue += hue_steps;
+    last_update = now;
   }
 
   for (uint8_t i = 0; i < LED_COUNT; i++) {
-    uint16_t key_hue = rainbow_hue + 16 * (i / 4);
-    if (key_hue >= 255)          {
+    uint16_t key_hue = hue + 16 * (i / 4);
+    if (key_hue >= 255) {
       key_hue -= 255;
     }
-    cRGB rainbow = hsvToRgb(key_hue, rainbow_saturation, rainbow_value);
+    cRGB rainbow = hsvToRgb(key_hue, saturation, brightness);
     ::LEDControl.setCrgbAt(i, rainbow);
   }
-  rainbow_hue += rainbow_wave_steps;
-  if (rainbow_hue >= 255) {
-    rainbow_hue -= 255;
-  }
-}
-
-void LEDRainbowWaveEffect::brightness(byte brightness) {
-  rainbow_value = brightness;
-}
-
-void LEDRainbowWaveEffect::update_delay(byte delay) {
-  rainbow_update_delay = delay;
 }
 }
 
--- src/Kaleidoscope-LEDEffect-Rainbow.h
+++ src/Kaleidoscope-LEDEffect-Rainbow.h
@@ -7,52 +7,32 @@ namespace kaleidoscope {
 class LEDRainbowEffect : public LEDMode {
  public:
   LEDRainbowEffect(void) {}
-
-  void brightness(byte);
-  byte brightness(void) {
-    return rainbow_value;
-  }
-  void update_delay(byte);
-  byte update_delay(void) {
-    return rainbow_update_delay;
-  }
   void update(void) final;
 
+  int8_t hue_steps = 1;
+  uint8_t update_delay = 40;
+  uint8_t saturation = 255;
+  uint8_t brightness = 50;
+
  private:
-  uint16_t rainbow_hue = 0;   //  stores 0 to 614
-
-  uint8_t rainbow_steps = 1;  //  number of hues we skip in a 360 range per update
-  uint16_t rainbow_last_update = 0;
-  uint16_t rainbow_update_delay = 40; // delay between updates (ms)
-
-  byte rainbow_saturation = 255;
-  byte rainbow_value = 50;
+  uint8_t hue = 0;
+  uint16_t last_update = 0;
 };
 
 
 class LEDRainbowWaveEffect : public LEDMode {
  public:
   LEDRainbowWaveEffect(void) {}
-
-  void brightness(byte);
-  byte brightness(void) {
-    return rainbow_value;
-  }
-  void update_delay(byte);
-  byte update_delay(void) {
-    return rainbow_update_delay;
-  }
   void update(void) final;
 
+  int8_t hue_steps = 1;
+  uint8_t update_delay = 40;
+  uint8_t saturation = 255;
+  uint8_t brightness = 50;
+
  private:
-  uint16_t rainbow_hue = 0;  //  stores 0 to 614
-
-  uint8_t rainbow_wave_steps = 1;  //  number of hues we skip in a 360 range per update
-  uint16_t rainbow_last_update = 0;
-  uint16_t rainbow_update_delay = 40; // delay between updates (ms)
-
-  byte rainbow_saturation = 255;
-  byte rainbow_value = 50;
+  uint8_t hue = 0;
+  uint16_t last_update = 0;
 };
 }
 
