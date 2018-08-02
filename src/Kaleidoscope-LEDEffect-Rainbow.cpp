f45e891857e5446894eec7aacf25e8306f1f69fe
Shriramana Sharma <samjnaa@gmail.com>
2018-08-02 12:01:35 +0530

    add breathe option
    note that the variable update delay has been renamed since if breathing is on,
    LED updates aren't delayed and this is only the interval between hue updates

--- src/Kaleidoscope-LEDEffect-Rainbow.cpp
+++ src/Kaleidoscope-LEDEffect-Rainbow.cpp
@@ -4,14 +4,18 @@ namespace kaleidoscope {
 
 void LEDRainbowEffect::update(void) {
   uint16_t now = millis();
-  if ((now - last_update) < update_delay) {
-    return;
+  if ((now - last_hue_update) < hue_update_interval) {
+    if (!breathe) { // no updates either due to hue change or breathing
+      return;
+    }
   } else {
     hue += hue_steps;
-    last_update = now;
+    last_hue_update = now;
   }
 
-  cRGB rainbow = hsvToRgb(hue, saturation, brightness);
+  cRGB rainbow = breathe ?
+                 breath_compute_helper(hue, saturation, now) :
+                 hsvToRgb(hue, saturation, brightness);
   ::LEDControl.set_all_leds_to(rainbow);
 }
 
@@ -19,11 +23,13 @@ void LEDRainbowEffect::update(void) {
 
 void LEDRainbowWaveEffect::update(void) {
   uint16_t now = millis();
-  if ((now - last_update) < update_delay) {
-    return;
+  if ((now - last_hue_update) < hue_update_interval) {
+    if (!breathe) { // no updates either due to hue change or breathing
+      return;
+    }
   } else {
     hue += hue_steps;
-    last_update = now;
+    last_hue_update = now;
   }
 
   for (uint8_t i = 0; i < LED_COUNT; i++) {
@@ -31,10 +37,13 @@ void LEDRainbowWaveEffect::update(void) {
     if (key_hue >= 255) {
       key_hue -= 255;
     }
-    cRGB rainbow = hsvToRgb(key_hue, saturation, brightness);
+    cRGB rainbow = breathe ?
+                   breath_compute_helper(key_hue, saturation, now) :
+                   hsvToRgb(key_hue, saturation, brightness);
     ::LEDControl.setCrgbAt(i, rainbow);
   }
 }
+
 }
 
 kaleidoscope::LEDRainbowEffect LEDRainbowEffect;
--- src/Kaleidoscope-LEDEffect-Rainbow.h
+++ src/Kaleidoscope-LEDEffect-Rainbow.h
@@ -10,13 +10,14 @@ class LEDRainbowEffect : public LEDMode {
   void update(void) final;
 
   int8_t hue_steps = 1;
-  uint8_t update_delay = 40;
+  uint8_t hue_update_interval = 40;
   uint8_t saturation = 255;
+  bool breathe = false;
   uint8_t brightness = 50;
 
  private:
   uint8_t hue = 0;
-  uint16_t last_update = 0;
+  uint16_t last_hue_update = 0;
 };
 
 
@@ -26,13 +27,14 @@ class LEDRainbowWaveEffect : public LEDMode {
   void update(void) final;
 
   int8_t hue_steps = 1;
-  uint8_t update_delay = 40;
+  uint8_t hue_update_interval = 40;
   uint8_t saturation = 255;
+  bool breathe = false;
   uint8_t brightness = 50;
 
  private:
   uint8_t hue = 0;
-  uint16_t last_update = 0;
+  uint16_t last_hue_update = 0;
 };
 }
 
